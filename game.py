79353fb40b743a763a20eee8ff211eb86224f5ad
coderock100 <vd.ssaathvik@gmail.com>
2025-12-17 17:42:15 +0530

    tic tac toe

--- game.py
+++ game.py
@@ -1,65 +1,101 @@
+import random
 import colorama
 from colorama import Fore, Style
 
+colorama.init(autoreset=True)
+
+# ---------- GAME LOGIC ----------
 def check_win(board, symbol):
     win_conditions = [
-        (0, 1, 2), (3, 4, 5), (6, 7, 8), 
-        (0, 3, 6), (1, 4, 7), (2, 5, 8), # 
-        (0, 4, 8), (2, 4, 6)             
+        (0,1,2), (3,4,5), (6,7,8),
+        (0,3,6), (1,4,7), (2,5,8),
+        (0,4,8), (2,4,6)
     ]
-    for cond in win_conditions:
-        if board[cond[0]] == board[cond[1]] == board[cond[2]] == symbol:
+    for a, b, c in win_conditions:
+        if board[a] == board[b] == board[c] == symbol:
             return True
     return False
 
 def check_full(board):
     return all(not spot.isdigit() for spot in board)
 
+# ---------- DISPLAY ----------
+def display_board(board):
+    print()
+    print(Fore.CYAN + f" {board[0]} | {board[1]} | {board[2]}")
+    print(Fore.CYAN + "---|---|---")
+    print(Fore.CYAN + f" {board[3]} | {board[4]} | {board[5]}")
+    print(Fore.CYAN + "---|---|---")
+    print(Fore.CYAN + f" {board[6]} | {board[7]} | {board[8]}")
+    print()
+
+# ---------- PLAYER SETUP ----------
+def player_choice():
+    choice = ''
+    while choice not in ['X', 'O']:
+        choice = input(Fore.GREEN + "Choose X or O: " + Style.RESET_ALL).upper()
+    if choice == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+# ---------- MOVES ----------
+def player_move(board, symbol):
+    position = ''
+    while position not in [str(i) for i in range(1,10)] or not board[int(position)-1].isdigit():
+        position = input(Fore.YELLOW + "Choose a position (1-9): " + Style.RESET_ALL)
+    board[int(position)-1] = symbol
+
+def ai_move(board, symbol):
+    available = [i for i in range(9) if board[i].isdigit()]
+    move = random.choice(available)
+    board[move] = symbol
+    print(Fore.RED + f"AI chose position {move + 1}")
+
+# ---------- MAIN GAME ----------
 def tic_tac_toe():
-    print("Welcome to Tic-Tac-Toe!")
+    print(Fore.MAGENTA + "🎮 Welcome to Tic-Tac-Toe 🎮")
     player_name = input(Fore.GREEN + "Enter your name: " + Style.RESET_ALL)
+
     while True:
-        board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        board = ['1','2','3','4','5','6','7','8','9']
         player_symbol, ai_symbol = player_choice()
         turn = 'Player'
         game_on = True
 
         while game_on:
-            dislay_board(board)
+            display_board(board)
+
             if turn == 'Player':
                 player_move(board, player_symbol)
                 if check_win(board, player_symbol):
-                    display(board)
-                    print(Fore.GREEN + f"Congratulations {player_name}, you win!" + Style.RESET_ALL)
+                    display_board(board)
+                    print(Fore.GREEN + f"🎉 Congratulations {player_name}, you win!")
+                    game_on = False
+                elif check_full(board):
+                    display_board(board)
+                    print(Fore.YELLOW + "😐 It's a draw!")
                     game_on = False
                 else:
-                    if check_full(board):
-                        display(board)
-                        print(Fore.YELLOW + "It's a draw!" + Style.RESET_ALL)
-                        game_on = False
-                    else:
-                        turn = 'AI'
+                    turn = 'AI'
             else:
                 ai_move(board, ai_symbol)
                 if check_win(board, ai_symbol):
-                    display(board)
-                    print(Fore.RED + "AI wins! Better luck next time." + Style.RESET_ALL)
+                    display_board(board)
+                    print(Fore.RED + "💀 AI wins! Better luck next time.")
+                    game_on = False
+                elif check_full(board):
+                    display_board(board)
+                    print(Fore.YELLOW + "😐 It's a draw!")
                     game_on = False
                 else:
-                    if check_full(board):
-                        display(board)
-                        print(Fore.YELLOW + "It's a draw!" + Style.RESET_ALL)
-                        game_on = False
-                    else:
-                        turn = 'Player'
-        player_again = input(Fore.CYAN + "Do you want to play again? (y/n): " + Style.RESET_ALL).lower()
-        if player_again == 'yes' or player_again == 'y':
-            print(Fore.MAGENTA + "Starting a new game..." + Style.RESET_ALL)
-            continue
-        else:
-            print(Fore.MAGENTA + "Thanks for playing! Goodbye!" + Style.RESET_ALL)
+                    turn = 'Player'
+
+        again = input(Fore.CYAN + "Play again? (y/n): " + Style.RESET_ALL).lower()
+        if again not in ['y', 'yes']:
+            print(Fore.MAGENTA + "Thanks for playing! 👋")
             break
 
+# ---------- RUN ----------
 if __name__ == "__main__":
-    colorama.init()
     tic_tac_toe()
